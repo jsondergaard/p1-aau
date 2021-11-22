255966a9564e89bb9715be8d0f16e47500958a32
Jacob Søndergaard <jacob@cavas.dk>
2021-11-22 10:43:59 +0100

    calendar: leap year add return

--- src/calendar/calendar.c
+++ src/calendar/calendar.c
@@ -2,9 +2,11 @@ int isLeapYear(int year) {
     if(year % 4 == FALSE && year % 100 != FALSE || year % 400 == FALSE)
     {
         // It is a leap year and February has 29 days.
+        return 1;
     }
     else
     {
         // It is not a leap year, so February has 28 days.
+        return 0;
     }
 }
