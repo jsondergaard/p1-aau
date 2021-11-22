03218d97847d4be661f6b967f38f1abb7ec379d5
Jacob Søndergaard <jacob@cavas.dk>
2021-11-22 09:40:36 +0100

    init

---
+++ cmake-build-debug/version.h
@@ -0,0 +1,62 @@
+/*
+
+	version.h -- C-Template
+
+	Copyright © 2015-2019 Fletcher T. Penney.
+
+
+	
+
+*/
+
+/**
+
+@file
+
+@brief Boilerplate c project with cmake support, CuTest unit testing, and more. - project version header
+
+**/
+
+
+#ifndef FILE_C-TEMPLATE_H
+#define FILE_C-TEMPLATE_H
+
+#define C-TEMPLATE_NAME "C-Template"
+
+#define C-TEMPLATE_VERSION "1.0.9"
+#define C-TEMPLATE_COPYRIGHT "Copyright © 2015-2019 Fletcher T. Penney."
+
+#define C-TEMPLATE_LICENSE "\tThe `c-template` project is released under the MIT License.\n"\
+"	\n"\
+"	GLibFacade.c and GLibFacade.h are from the MultiMarkdown v4 project:\n"\
+"	\n"\
+"		https://github.com/fletcher/MultiMarkdown-4/\n"\
+"	\n"\
+"	MMD 4 is released under both the MIT License and GPL.\n"\
+"	\n"\
+"	\n"\
+"	CuTest is released under the zlib/libpng license. See CuTest.c for the text\n"\
+"	of the license.\n"\
+"	\n"\
+"	\n"\
+"	## The MIT License ##\n"\
+"	\n"\
+"	Permission is hereby granted, free of charge, to any person obtaining a copy\n"\
+"	of this software and associated documentation files (the \"Software\"), to deal\n"\
+"	in the Software without restriction, including without limitation the rights\n"\
+"	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"\
+"	copies of the Software, and to permit persons to whom the Software is\n"\
+"	furnished to do so, subject to the following conditions:\n"\
+"	\n"\
+"	The above copyright notice and this permission notice shall be included in\n"\
+"	all copies or substantial portions of the Software.\n"\
+"	\n"\
+"	THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"\
+"	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"\
+"	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"\
+"	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"\
+"	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"\
+"	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n"\
+"	THE SOFTWARE."
+
+#endif
