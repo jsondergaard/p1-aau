c83a40e02f402cc79e6acdbdb8eb86f03cce86e7
Nichlas Hjorth <nhjor21@student.aau.dk>
2021-11-26 10:41:42 +0100

    I change math

--- src/main.c
+++ src/main.c
@@ -70,8 +70,31 @@ DString * scan_file(char * fname) {
 	return buffer;
 }
 
+void calender(int calenderArray[][], int rows, int collums);
+
 int main( int argc, char** argv ) {
 	/* Make your program do whatever you want */
-
-    printf("Hello World");
+    int const collums = 7;
+    int const rows = 5;
+    int calenderArray[rows][collums]={
+            {1,2,3,4,5,6,7},
+            {5,6,7,8,9,10,11},
+            {12,13,14,15,16,17,18},
+            {19,20,21,22,23,24,25},
+            {26,27,28,29,30,31,0}
+    };
+    calender(calenderArray[][], rows, collums);
 }
+
+void calender(int calenderArray[][], int rows, int collums){
+
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < collums; j++){
+            printf("%d ", calenderArray[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
