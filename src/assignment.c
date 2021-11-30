e7feb8d86970fdda33c2e9e30f29d94aa0455120
Jacob Søndergaard <jacob@cavas.dk>
2021-11-30 12:51:28 +0100

    Bootstrap addAssignment

--- src/assignment.c
+++ src/assignment.c
@@ -4,16 +4,27 @@
 int addAssignment(void)
 {
 	int c;
+	char *title;
 
 	do
 	{
-		printf("... add assignment ...\n");
+		printf(YELLOW "Add assignment\n" RESET);
+
+		printf("What do you want the assignment to be called? > ");
+		scanf("%s", &title);
+		printf("\n");
+
+#ifndef NDEBUG
+		printf("Assignment name: %s\n", &title);
+#endif
 
 		printf("Press " RED "m" RESET " to return to menu\n");
 
 		c = getchar();
 		getchar();
 
-		return 0;
-	} while (c != 'q');
+		return 1;
+	} while (c != 'm');
+
+	return 0;
 }
