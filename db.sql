a45d35c51feecfe049c83c097bdbd604247c8428
Jacob Søndergaard <j.sondergaard@me.com>
2021-12-14 15:42:33 +0100

    db changes

--- db.sql
+++ db.sql
@@ -2,7 +2,6 @@ DROP TABLE assignments;
 CREATE TABLE assignments (
 	id integer PRIMARY KEY,
 	title varchar NOT NULL,
-	original_due_at date NOT NULL,
 	due_at date NOT NULL,
 	due_at_time time NOT NULL,
 	student_time integer NOT NULL,
@@ -10,7 +9,7 @@ CREATE TABLE assignments (
 	updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
  );
 
-INSERT INTO "assignments" ("id", "title", "original_due_at", "due_at", "due_at_time", "student_time", "created_at", "updated_at") VALUES
+INSERT INTO "assignments" ("id", "title", "due_at", "due_at_time", "student_time", "created_at", "updated_at") VALUES
 ('1', 'Test', '2021-12-24', '2021-12-24', '12:00', '10', '2021-12-14 09:21:01', '2021-12-14 09:21:01'),
 ('2', 'Test 2', '2021-12-26', '2021-12-26', '14:00', '5', '2021-12-14 09:21:47', '2021-12-14 09:21:47'),
 ('3', 'Test 3', '2021-12-24', '2021-12-24', '12:00', '48', '2021-12-14 09:21:47', '2021-12-14 09:21:47');
--- src/assignment.c
+++ src/assignment.c
@@ -6,12 +6,12 @@
 #include <sqlite3.h>
 #include "defines.h"
 
-typedef struct{
-
-	int day;
-	int studentTime;
-
-}Day;
+// typedef struct{
+// 	int year;
+// 	int month;
+// 	int day;
+// 	int studentTime;
+// } Day;
 
 int addAssignment(void);
 int listAssignments(void);
@@ -46,11 +46,12 @@ int assignmentMenu(void)
 int addAssignment(void)
 {
 
-	Day day[31];
+	// Day days[31];
 
 	char title[128];
 	char rangeStart[64];
 	char rangeEnd[64];
+	char dueDate[64];
 	char dueDateTime[64];
 
 	time_t rawtime;
@@ -58,17 +59,12 @@ int addAssignment(void)
 	char timeString[64];
 	int studentTime = 0;
 
-	for (int i = 1; i <= 31; i++)
-	{
-		day[i].day = i;
-		day[i].studentTime = 0;
-	}
-
 	time(&rawtime);
 	strftime(timeString, 64, "%Y-%m-%d %H:%M:%S", localtime(&rawtime));
 
 	sqlite3 *db;
 	sqlite3_stmt *res;
+	sqlite3_stmt *res2;
 	char *error = 0;
 
 	int rc = sqlite3_open(DBFILE, &db);
@@ -106,56 +102,42 @@ int addAssignment(void)
 		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@start"), rangeStart, sizeof(rangeStart), SQLITE_STATIC);
 		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@end"), rangeEnd, sizeof(rangeEnd), SQLITE_STATIC);
 	}
+
 	ERRCHECK
 
 	while (sqlite3_step(res) != SQLITE_DONE)
 	{
-		int dayGet, dummy;
-		sscanf(sqlite3_column_text(res,0), "%d-%d-%d", &dummy, &dummy, &dayGet);
-		day[dayGet].studentTime+=sqlite3_column_int(res,1);
+		printf(RED "%s: %d" RESET " student hours, don't pick this\n", sqlite3_column_text(res, 0), sqlite3_column_int(res, 1));
 	}
 
-	for (int i = 1; i <= 31; i++)
-	{
-		int count=0,lowest = 10;
-		
-		if(day[i].studentTime < lowest)
-			lowest = day[i].studentTime;
+	sqlite3_finalize(res);
 
-		if(day[i].studentTime == 0){
-			printf("%d: %d\n", i, day[i].studentTime);
-			count++;
-		}
+	printf("\n");
+	printf("So when will it be? (YYYY-MM-DD)\n> ");
+	scanf(" %[^\n]", &dueDate[0]);
+	printf("\n");
 
-		if (count == 0)
-		{
-			
-		}
-		
-	}
-
-	/*char *sql = "INSERT INTO assignments(title, original_due_at, due_at, buffer_time, student_time, created_at, updated_at) VALUES(@title, @dueDate, @dueDate, @bufferTime, @studentTime, @createdAt, @updatedAt);";
-	rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+	char *sql2 = "INSERT INTO assignments(title, due_at, due_at_time, student_time, created_at, updated_at) VALUES(@title, @dueDate, @dueDateTime, @studentTime, @createdAt, @updatedAt);";
+	rc = sqlite3_prepare_v2(db, sql2, -1, &res2, 0);
 	if (rc == SQLITE_OK)
 	{
-		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@title"), title, sizeof(title), SQLITE_STATIC);
-		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@dueDate"), dueDate, sizeof(dueDate), SQLITE_STATIC);
-		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@dueDate"), dueDate, sizeof(dueDate), SQLITE_STATIC);
-		sqlite3_bind_int(res, sqlite3_bind_parameter_index(res, "@bufferTime"), bufferTime);
-		sqlite3_bind_int(res, sqlite3_bind_parameter_index(res, "@studentTime"), studentTime);
-		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@createdAt"), timeString, sizeof(timeString), SQLITE_STATIC);
-		sqlite3_bind_text(res, sqlite3_bind_parameter_index(res, "@updatedAt"), timeString, sizeof(timeString), SQLITE_STATIC);
+		sqlite3_bind_text(res2, sqlite3_bind_parameter_index(res2, "@title"), title, sizeof(title), SQLITE_STATIC);
+		sqlite3_bind_text(res2, sqlite3_bind_parameter_index(res2, "@dueDate"), dueDate, sizeof(dueDate), SQLITE_STATIC);
+		sqlite3_bind_text(res2, sqlite3_bind_parameter_index(res2, "@dueDateTime"), dueDateTime, sizeof(dueDateTime), SQLITE_STATIC);
+		sqlite3_bind_int(res2, sqlite3_bind_parameter_index(res2, "@studentTime"), studentTime);
+		sqlite3_bind_text(res2, sqlite3_bind_parameter_index(res2, "@createdAt"), timeString, sizeof(timeString), SQLITE_STATIC);
+		sqlite3_bind_text(res2, sqlite3_bind_parameter_index(res2, "@updatedAt"), timeString, sizeof(timeString), SQLITE_STATIC);
 	}
 
 	ERRCHECK
 
-	rc = sqlite3_step(res);
+	rc = sqlite3_step(res2);
 	if (rc == SQLITE_DONE)
 		printf(GREEN "Successfully created assignment.\n" RESET);
 	else if (rc != SQLITE_DONE)
-		printf(RED "Failed to create assignment.\n" RESET); */
+		printf(RED "Failed to create assignment.\n" RESET);
 
-	sqlite3_finalize(res);
+	sqlite3_finalize(res2);
 	sqlite3_close(db);
 
 	printf("Press ENTER to continue..\n");
