8ac54427c80758ad41d09ec938a2a7421579d066
Jacob Søndergaard <j.sondergaard@me.com>
2021-12-14 13:08:29 +0100

    db mofo

--- db.sql
+++ db.sql
@@ -5,21 +5,12 @@ CREATE TABLE assignments (
 	original_due_at date NOT NULL,
 	due_at date NOT NULL,
 	due_at_time time NOT NULL,
-	buffer_time integer NOT NULL,
 	student_time integer NOT NULL,
 	created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
- )
+ );
 
---Create before update and after insert triggers:
--- CREATE TRIGGER UPDATE_ASSIGNMENTS BEFORE UPDATE ON assignments
---     BEGIN
---        UPDATE assignments SET updated_at = datetime('now', 'localtime')
---        WHERE rowid = new.rowid;
---     END
-
--- CREATE TRIGGER INSERT_ASSIGNMENTS AFTER INSERT ON assignments
---     BEGIN
---        UPDATE assignments SET created_at = datetime('now', 'localtime')
---        WHERE rowid = new.rowid;
---     END
+INSERT INTO "assignments" ("id", "title", "original_due_at", "due_at", "due_at_time", "student_time", "created_at", "updated_at") VALUES
+('1', 'Test', '2021-12-24', '2021-12-24', '12:00', '10', '2021-12-14 09:21:01', '2021-12-14 09:21:01'),
+('2', 'Test 2', '2021-12-26', '2021-12-26', '14:00', '5', '2021-12-14 09:21:47', '2021-12-14 09:21:47'),
+('3', 'Test 3', '2021-12-24', '2021-12-24', '12:00', '48', '2021-12-14 09:21:47', '2021-12-14 09:21:47');
